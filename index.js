6cc0629208ca581c1d98c59d52484dd945ef9f6f
sobuj301 <sobuj42301@gmail.com>
2024-01-08 23:24:07 +0600

    curd operation start get services

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require('dotenv').config()
+
 const port = process.env.PORT || 5000;
 
 
 
 // middleware
+
 app.use(cors())
 app.use(express.json())
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.udy85xl.mongodb.net/?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
@@ -25,7 +28,15 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     
+    const serviceCollection = client.db('green-shop').collection('services')
     await client.connect();
+
+    app.get('/services',async(req,res) =>{
+        const cursor = serviceCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+    
     
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
