43f66641d14abcd48dd022e63eba6f2133071b49
sobuj301 <sobuj42301@gmail.com>
2024-01-12 02:36:56 +0600

    implement search functionality by the service name

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 
 const port = process.env.PORT || 5000;
@@ -27,31 +27,134 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    
+
     const serviceCollection = client.db('green-shop').collection('services')
+    const bookingCollection = client.db('green-shop').collection('booking')
     await client.connect();
 
-    app.get('/services',async(req,res) =>{
-        const cursor = serviceCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
+
+
+
+    app.get('/services', async (req, res) => {
+      // console.log(req.query.service_name)
+      let query = {};
+      // let sortObj={};
+      const serviceName = req.query.service_name;
+      // const sortField = req.query.sortField;
+      // const sortOrder = req.query.sortOrder;
+      let option = {}
+      if (req.query.email) {
+        option = {
+          email: req.params.email
+        }
+      }
+
+      if (serviceName) {
+        query = {
+          service_name: req.query.service_name
+        }
+      }
+      // if(sortField && sortOrder){
+      //    sortObj[sortField]=sortOrder
+      // }
+      const cursor = serviceCollection.find(query)
+      const result = await cursor.toArray()
+      res.send(result)
     })
-    
-    
+
+
+    app.get('/service/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await serviceCollection.findOne(query);
+      res.send(result)
+    })
+
+    app.get("/book/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id)
+      const query = { _id: new ObjectId(id) };
+      const result = await serviceCollection.findOne(query)
+      res.send(result)
+    })
+
+
+    // ADD SERVICE
+
+    app.post('/addService', async (req, res) => {
+      const service = req.body;
+      const result = await serviceCollection.insertOne(service)
+      res.send(result)
+    })
+
+
+    // MANAGE SERVICES
+    app.get('/allServices', async (req, res) => {
+      const email = req.query.email;
+      const query ={email:email}
+      
+      const cursor = serviceCollection.find(query)
+      const result = await cursor.toArray()
+      res.send(result)
+    })
+
+
+    // delete service
+
+    app.delete('/allServices/:id',async(req,res) =>{
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)}
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result)
+    })
+
+    // Update service
+
+    app.patch('/update/:id',async(req,res) =>{
+      const service = req.body;
+      const id = req.params.id;
+      const filter = {_id:new ObjectId(id)};
+      const options = { upsert: true };
+      const update ={
+        $set:{
+          serviceName:service.serviceName,
+          photo:service.photo,
+          email:service.email,
+          price:service.price,
+          name:service.name,
+          area:service.area,
+          description:service.description
+        }
+      }
+      const result= await serviceCollection.updateOne(filter,update,options);
+      res.send(result)
+      
+    })
+
+
+    // booking service related 
+
+    app.post('/bookings', async (req, res) => {
+      const booking = req.body;
+      console.log(booking)
+      const result = await bookingCollection.insertOne(booking)
+      res.send(result)
+    })
+
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
-  
+
     // await client.close();
   }
 }
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('Green Shop Server Is Running..................')
+  res.send('Green Shop Server Is Running..................')
 })
 
 app.listen(port, () => {
-    console.log(`running port ${port}`)
+  console.log(`running port ${port}`)
 })
 
